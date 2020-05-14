3b6ff623df6cec4f47cd6fba4808e953c2c6d92c
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-05-14 11:32:12 -0700

    ex5

--- drills.js
+++ drills.js
@@ -1,3 +1,5 @@
+const {LinkedList, display} = require('./linkedList');
+
 /*
 1. Understanding merge sort
 Given the following list of numbers 21, 1, 26, 45, 29, 28, 2, 9, 
@@ -25,7 +27,7 @@ function mergeSort(array) {   // 1, 21  26, 45  28, 29  2, 9      16, 49  27, 39
   //console.log(right);
   left = mergeSort(left); // 21
   right = mergeSort(right);  // 1
-  console.log(merge(left, right, array));
+  //console.log(merge(left, right, array));
   return merge(left, right, array); // 1, 21
 }
 
@@ -95,37 +97,54 @@ function merge(left, right, array) {
 
 function quickSort(array, start = 0, end = array.length) {
   if (start >= end) {
-      return array;
+    return array;
   }
   const middle = partition(array, start, end);
-  console.log('middle:', middle)
   array = quickSort(array, start, middle);
-  console.log('array fist half', array)
   array = quickSort(array, middle + 1, end);
-  console.log('array second half', array)
   return array;
-};
+}
 
 function partition(array, start, end) {
   const pivot = array[end - 1];
-  console.log('pivot', pivot)
   let j = start;
   for (let i = start; i < end - 1; i++) {
-      if (array[i] <= pivot) {
-          swap(array, i, j);
-          j++;
-      }
+    if (array[i] <= pivot) {
+      swap(array, i, j);
+      j++;
+    }
   }
   swap(array, end-1, j);
   return j;
-};
+}
 
 function swap(array, i, j) {
   const tmp = array[i];
   array[i] = array[j];
   array[j] = tmp;
-};
+}
 
 //quickSort([3, 9, 1, 14, 17, 24, 22, 20])
 
-quickSort([14, 17, 13, 15, 19, 10, 3, 16, 9, 12]) 
+// quickSort([14, 17, 13, 15, 19, 10, 3, 16, 9, 12]); 
+
+// 3. Implementing quicksort
+
+function main() {
+  let dataString = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
+  const data = dataString.split(' ').map((el) => Number(el));
+  // console.log('Quick sort:', quickSort(data));
+  // console.log('Merge Sort:', mergeSort(data));
+
+  const SLL = new LinkedList();
+  SLL.insertLast(24);
+  SLL.insertLast(5);
+  SLL.insertLast(21);
+  SLL.insertLast(50);
+  SLL.insertLast(13);
+
+  //console.log(display(SLL));
+  console.log(mergeSort(display(SLL)));
+}
+
+main();
---
+++ linkedList.js
@@ -0,0 +1,190 @@
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(item) {
+    this.head = new _Node(item, this.head);
+  }
+
+  insertLast(item) {
+    if (this.head === null) {
+      this.insertFirst(item);
+    }
+    else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }                        
+      tempNode.next = new _Node(item, null);
+    }
+  }
+
+  find(item) { 
+    // Start at the head
+    let currNode = this.head;
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // Check for the item 
+    while (currNode.value !== item) {
+      /* Return null if it's the end of the list 
+           and the item is not on the list */
+      if (currNode.next === null) {
+        return null;
+      }
+      else {
+        // Otherwise, keep looking 
+        currNode = currNode.next;
+      }
+    }
+    // Found it
+    return currNode;
+  }
+
+  remove(item){ 
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // If the node to be removed is head, make the next node head
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+    // Start at the head
+    let currNode = this.head;
+    // Keep track of previous
+    let previousNode = this.head;
+
+    while ((currNode !== null) && (currNode.value !== item)) {
+      // Save the previous node 
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      console.log('Item not found');
+      return;
+    }
+    previousNode.next = currNode.next;
+  }
+
+  insertBefore(itemToInsert, itemBefore) {
+    if (this.head === null || this.head.value === itemBefore) {
+      this.insertFirst(itemToInsert);
+    } else {
+
+      let currNode = this.head;
+      let previousNode = null;
+      while ((currNode !== null) && (currNode.value !== itemBefore)) {
+        // Save the previous node 
+        previousNode = currNode;
+        currNode = currNode.next;
+      }
+      if (null !== currNode) {
+        previousNode.next = new _Node(itemToInsert, currNode);
+      } else {
+        this.insertLast(itemToInsert);
+      }
+    }
+  }
+
+  insertAfter(itemBefore, itemToInsert) { 
+    if (this.head === null) 
+      this.insertFirst(itemToInsert);
+    else {
+  
+      let new_node = new _Node(itemToInsert); 
+      let prev_node = this.find(itemBefore);  
+
+      new_node.next = prev_node.next; 
+      prev_node.next = new_node; 
+    } 
+
+  }
+
+
+  insertAt(itemToInsert, position) {
+    //create new node.
+    let node = new _Node();
+    node.value = itemToInsert;
+    node.next = null;
+
+    if (this.head === null) {
+      //if head is null and position is zero then exit.
+      if (position !== 0) {
+        return;
+      } else { //node set to the head.
+        this.head = node;
+      }
+    }
+
+    if (this.head !== null && position === 0) {
+      node.next = this.head;
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+    let previous = null;
+
+    let i = 0;
+
+    while (i < position) {
+      previous = current;
+      current = current.next;
+      if (current === null) {
+        break;
+      }
+      i++;
+    }
+
+    node.next = current;
+    previous.next = node;
+  }
+  
+
+}
+
+function display(linkedList) {
+  let arr = [];
+  if(linkedList.head.value === null) {
+    return [];
+  }
+  //while 
+  let current = linkedList.head;
+  while(current !== null) {
+    arr.push(current.value);
+    current = current.next;
+  }
+  return arr;
+}
+
+function size(linkedList) {
+  console.log('Size:', display(linkedList).length);
+}
+
+function isEmpty(linkedList) {
+  return linkedList.head === null;
+}
+
+//Boomer
+
+module.exports={LinkedList, display};
+
+
+
+
+
+
+
+
+
